a545bef8ca3987828fd22241309baccaf5108709
automatedcommunication <aletheia@aletheias-MacBook-Air.local>
2018-07-17 01:02:45 -0700

    added dynamic background color styling

--- src/components/Feelsbot.vue
+++ src/components/Feelsbot.vue
@@ -1,34 +1,60 @@
 <template>
-  <div class="feelsbot">
-    <h1>{{ msg }}</h1>
-  </div>
+  <div id="app" v-bind:style="{backgroundColor: color}">
+      <div class="color">
+      <input type="text" v-bind:style="{color: color}" v-model="color" placeholder="enter a color"/>
+      </div>
+    </div>
 </template>
 
 <script>
 export default {
+  el: app,
   name: 'Feelsbot',
   data () {
     return {
-      msg: 'Hello, I am FeelsBot. Tell me anything you want and I tell you how it makes me feel.'
+      color: ''
     }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
+<style>
+@import url("https://fonts.googleapis.com/css?family=Proza+Libre|Fira+Mono");
+html, body {
+  height: 100%;
+  margin: 0;
   padding: 0;
+  background: black;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+#app {
+  display: flex;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
 }
-a {
-  color: #42b983;
+
+#app input {
+  padding: 1rem 1rem;
+  font-size: 2rem;
+  border: 0;
+  border-radius: 3px;
+  background: #fff;
+}
+
+h1 {
+  font-family: 'Proza Libre', sans-serif;
+  color: #fff;
+  font-weight: 300;
+}
+
+input[type="color"] {
+  width: 40px;
+  border: 1px solid black;
+  margin: 0px;
+  padding: 0px !important;
+  height: 40px;
 }
 </style>
