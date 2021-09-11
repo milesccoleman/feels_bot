fb29d1e575233a789624b7643ec8295559e05f82
milesccoleman <milesccoleman@gmail.com>
2021-09-11 02:41:22 -0400

    trying new config

--- src/router/index.js
+++ src/router/index.js
@@ -8,7 +8,7 @@ Vue.use(Router)
 export default new Router({
   routes: [
     {
-      path: 'milesccoleman.com/',
+      path: '/Feelsbot',
       name: 'Feelsbot',
       component: Feelsbot
     },
